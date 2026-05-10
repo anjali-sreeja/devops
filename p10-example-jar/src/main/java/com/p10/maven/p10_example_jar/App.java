afd952e19eec219c513f758a9fdebdbbde0327d7
anjali-sreeja <anjalisreeja435@gmail.com>
2026-05-10 23:47:47 +0530

    1st commit

---
+++ p10-example-jar/src/main/java/com/p10/maven/p10_example_jar/App.java
@@ -0,0 +1,13 @@
+package com.p10.maven.p10_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
